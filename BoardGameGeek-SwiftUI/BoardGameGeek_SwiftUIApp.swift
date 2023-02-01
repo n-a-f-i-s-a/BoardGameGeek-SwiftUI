6eb8a0183530440760a7d5bdad502b115d9a10f2
n-a-f-i-s-a <nafisa.rahman@ittybittyapps.com>
2023-02-02 10:21:35 +1100

    Initial Commit

---
+++ BoardGameGeek-SwiftUI/BoardGameGeek_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoardGameGeek_SwiftUIApp.swift
+//  BoardGameGeek-SwiftUI
+//
+//  Created by Nafisa Rahman on 2/2/2023.
+//
+
+import SwiftUI
+
+@main
+struct BoardGameGeek_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
